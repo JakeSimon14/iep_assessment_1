9dd7f07b420b654b5f237270c16b5018077c6e4f
Jake Simon <jake.simon@nestgroup.net>
2025-06-07 02:42:28 +0530

    updated login and logout screens

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,30 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule,FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LabelModule } from '@progress/kendo-angular-label';
 import { InputsModule, TextBoxModule } from '@progress/kendo-angular-inputs';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
-import { FilterItem } from '../../models/filter-item.model';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule,LabelModule,InputsModule,ButtonsModule,TextBoxModule],
+  imports: [ReactiveFormsModule, CommonModule, LabelModule, InputsModule, ButtonsModule, TextBoxModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent{
-  loginForm! : FormGroup;
-  error = '';
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  loginFailed = false;
+  submitted = false;
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
     private router: Router
   ) {}
-
 
   ngOnInit() {
     this.loginForm = this.fb.group({
@@ -35,23 +34,19 @@ export class LoginComponent{
   }
 
   login() {
-   const { ssoid, password } = this.loginForm.value;
-  if(this.loginForm.valid){
-    debugger;
-    if(this.authService.login(ssoid,password))
-      {
-        debugger
-        //this.showInvalidCredentials = true;
-        localStorage.setItem('isLoggedIn','true');
-        this.router.navigate(['/dashboard']);
-        return true;
-      }
-      else
-      {
-        //this.showInvalidCredentials = false;
-         return false;
-      }
+    this.submitted = true;
+    this.loginFailed = false;
+
+    if (this.loginForm.invalid) return;
+
+    const { ssoid, password } = this.loginForm.value;
+    const isAuthenticated = this.authService.login(ssoid, password);
+
+    if (isAuthenticated) {
+      localStorage.setItem('isLoggedIn', 'true');
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.loginFailed = true;
+    }
   }
-  return false;
-}
 }
